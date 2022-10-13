fb21960a2c99cc7846594cfd320acd53e0c3841e
gowtham116 <74004548+gowtham116@users.noreply.github.com>
2022-10-13 13:01:40 +0530

    admin

--- urls.py
+++ urls.py
@@ -1,15 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
+#from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('StockApp.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-    path('stocks/', include('stocks.urls')),
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	path('register/', UserRegisterView.as_view(), name='register'),
+	path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+	#path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
+	path('password/', PasswordsChangeView.as_view(),name='change_password'),
+	path('password_success', views.password_success, name="password_success"),
+	path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+	path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
+	path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
+]
